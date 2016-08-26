acaa914d97a4609924c969b096f914fa41be0c28
hutdev <hutdevelopment@gmail.com>
2016-08-26 19:52:38 +0200

    CA certificate creation

--- gocert.go
+++ gocert.go
@@ -27,16 +27,37 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	"crypto/x509/pkix"
+	"flag"
 	"io/ioutil"
 	"log"
+	"math"
+	"math/big"
+	"os"
 	"path"
+	"time"
 )
 
 const defaultKeysize = 2048
+const defaultCAAge = 100
 const defaultOutpath = "."
 const defaultCaKeyName = "ca.key"
+const defaultCaCertName = "ca.crt"
 const defaultCertKeyName = "cert.key"
 const restrictivePermissions = 0600
+
+var commonName string
+
+func init() {
+	const cnUsage = "Value for the common name (CN) field"
+	const cnFlag = "cn"
+	if hostname, err := os.Hostname(); err == nil {
+		flag.StringVar(&commonName, cnFlag, hostname, cnUsage)
+	} else {
+		flag.StringVar(&commonName, cnFlag, "dummy", cnUsage)
+	}
+	flag.Parse()
+}
 
 func GenerateKey(path string) (*rsa.PrivateKey, error) {
 	if key, err := rsa.GenerateKey(rand.Reader, defaultKeysize); err == nil {
@@ -46,23 +67,57 @@ func GenerateKey(path string) (*rsa.PrivateKey, error) {
 	}
 }
 
+func CreateCACert(path string, cn string, key *rsa.PrivateKey) error {
+	if serial, serialErr := rand.Int(rand.Reader, big.NewInt(math.MaxInt64)); serialErr == nil {
+		template := x509.Certificate{
+			Subject: pkix.Name{
+				CommonName: cn,
+			},
+			SerialNumber: serial,
+			NotBefore:    time.Now(),
+			NotAfter:     time.Now().AddDate(defaultCAAge, 0, 0),
+			IsCA:         true,
+			KeyUsage:     x509.KeyUsageCertSign,
+		}
+		if cert, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key); err == nil {
+			ioutil.WriteFile(path, cert, restrictivePermissions)
+			return nil
+		} else {
+			return err
+		}
+	} else {
+		return serialErr
+	}
+}
+
 func main() {
 	outpath := defaultOutpath
 	caKeyName := defaultCaKeyName
 	caKeyPath := path.Join(outpath, caKeyName)
+	caCertName := defaultCaCertName
+	caCertPath := path.Join(outpath, caCertName)
 	certKeyName := defaultCertKeyName
 	certKeyPath := path.Join(outpath, certKeyName)
+	var caKey *rsa.PrivateKey
+	var err error
 
 	//Create a private key for the CA
-	if _, err := GenerateKey(caKeyPath); err == nil {
+	if caKey, err = GenerateKey(caKeyPath); err == nil {
 		log.Printf("Private key stored at %s.\n", caKeyPath)
 	} else {
 		log.Fatal(err)
 	}
 
 	//Create a private key for the certificate
-	if _, err := GenerateKey(certKeyPath); err == nil {
+	if _, err = GenerateKey(certKeyPath); err == nil {
 		log.Printf("Private key stored at %s.\n", certKeyPath)
+	} else {
+		log.Fatal(err)
+	}
+
+	//Create a self-signed CA certificate
+	if err = CreateCACert(caCertPath, commonName, caKey); err == nil {
+		log.Printf("CA certificate stored at %s.\n", caCertPath)
 	} else {
 		log.Fatal(err)
 	}
